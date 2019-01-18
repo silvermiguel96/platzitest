455b588591a42a0f47b51140b225178b4c99ee18
Miguelangel <silvermiguel96@gmail.com>
2019-01-17 22:59:07 -0500

    Exception Snapshot

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,4 +5,22 @@ describe('This time the snapshot', () => {
   test('Snapshot', () => {
     expect(getCharacter(rick)).toMatchSnapshot()
   })
+
+  test('Forever fail snapshot', () => {
+    const user = {
+      createAt: new Date(),
+      id: Math.floor(Math.random() * 20),
+    }
+    expect(user).toMatchSnapshot()
+  })
+
+  test('has an exception within the code', () => {
+    const user = {
+      id : Math.floor( Math.random() * 20),
+      name: "Miguelangel Rendon"
+    }
+    expect(user).toMatchSnapshot({
+      id: expect.any(Number)
+    })
+  })
 })
